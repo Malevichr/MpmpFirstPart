d7a1a0ba5a0e3ec9e509335a3383c9c221f09e17
Danila <rexerofficial@gmail.com>
2024-03-07 20:01:07 +0300

    Fourth lesson. FractionalNumber. The basic functionality is completed

--- src/lessons/lesson04/FractionalNumber.kt
+++ src/lessons/lesson04/FractionalNumber.kt
@@ -2,62 +2,135 @@ package lessons.lesson04
 
 import lessons.lesson02.Validator
 
-class FractionalNumberValidator(val fractionalNumber: String): Validator{
-    companion object {
-        val regexValidator = Regex("""\d+(/\d*)?""")
-    }
 
-    override fun validate(): Boolean {
-        if (!regexValidator.matches(fractionalNumber))
-            throw IllegalArgumentException("Not fraction in Integers")
-        else
-            return true
-    }
-
-}
-class FractionalNumber(fractionalNumber: String): Number(), Comparable<FractionalNumber> {
+class FractionalNumber(fractional: String) : Number(), Comparable<FractionalNumber> {
     private val numerator: Int
     private val denominator: Int
 
     init {
-        FractionalNumberValidator(fractionalNumber).validate()
+        FractionalNumberValidator(fractional).validate()
 
-        this.numerator = fractionalNumber.substringBefore("/").toInt()
-        this.denominator = if (fractionalNumber.contains("/")) fractionalNumber.substringAfter("/").toInt() else 1
+        try {
+            numerator = fractional.substringBefore("/").toInt()
+        } catch (_: Exception) {
+            throw (IllegalArgumentException("Numerator out of range"))
+        }
+        try {
+            this.denominator = if (fractional.contains("/")) fractional.substringAfter("/").toInt() else 1
+        } catch (_: Exception) {
+            throw (IllegalArgumentException("Denominator out of range"))
+        }
+        if (denominator == 0)
+            throw IllegalArgumentException("Denominator is zero")
+
     }
-    companion object{
 
+    constructor(fractionalNumber: FractionalNumber) : this(fractionalNumber.toString())
 
-    }
     override fun compareTo(other: FractionalNumber): Int {
-        TODO("Not yet implemented")
+        return this.toDouble().compareTo(other.toDouble())
     }
 
-    override fun toByte(): Byte {
-        TODO("Not yet implemented")
-    }
+    override fun toByte(): Byte = (numerator / denominator).toByte()
 
-    override fun toDouble(): Double {
-        TODO("Not yet implemented")
-    }
+    override fun toDouble(): Double = numerator.toDouble() / denominator
 
-    override fun toFloat(): Float {
-        TODO("Not yet implemented")
-    }
 
-    override fun toInt(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun toFloat(): Float = numerator.toFloat() / denominator
 
-    override fun toLong(): Long {
-        TODO("Not yet implemented")
-    }
 
-    override fun toShort(): Short {
-        TODO("Not yet implemented")
-    }
+    override fun toInt(): Int = numerator / denominator
+
+    override fun toLong(): Long = (numerator / denominator).toByte().toLong()
+
+    override fun toShort(): Short = (numerator / denominator).toByte().toShort()
 
     override fun toString(): String {
         return "$numerator/$denominator"
+    }
+
+    fun shortenFraction(): FractionalNumber {
+        var gcd = getGreatestCommonDivisor(numerator, denominator)
+        if ((numerator < 0) and (denominator > 0))
+            gcd *= -1
+        return FractionalNumber((numerator / gcd).toString() + "/" + (denominator / gcd))
+    }
+
+    private fun getGreatestCommonDivisor(a: Int, b: Int): Int {
+        var num1 = a
+        var num2 = b
+        while (num2 != 0) {
+            val temp = num2
+            num2 = num1 % num2
+            num1 = temp
+        }
+        return num1
+    }
+
+    operator fun plus(other: FractionalNumber): FractionalNumber {
+        val denominator = this.denominator * other.denominator
+        val numerator = this.numerator * other.denominator + other.numerator * this.denominator
+        return FractionalNumber("$numerator/$denominator").shortenFraction()
+    }
+
+    
+
+    operator fun minus(other: FractionalNumber): FractionalNumber {
+        val denominator = this.denominator * other.denominator
+        val numerator = this.numerator * other.denominator - other.numerator * this.denominator
+        return FractionalNumber("$numerator/$denominator").shortenFraction()
+    }
+
+    operator fun div(other: FractionalNumber): FractionalNumber {
+        val numerator = this.numerator * other.denominator
+        val denominator = this.denominator * other.numerator
+        return FractionalNumber("$numerator/$denominator").shortenFraction()
+    }
+
+    operator fun times(other: FractionalNumber): FractionalNumber {
+        val numerator = this.numerator * other.numerator
+        val denominator = this.denominator * other.denominator
+        return FractionalNumber("$numerator/$denominator").shortenFraction()
+    }
+
+    operator fun unaryPlus(): FractionalNumber {
+        return FractionalNumber(this)
+    }
+
+    operator fun unaryMinus(): FractionalNumber {
+        return (FractionalNumber(this) * FractionalNumber("-1")).shortenFraction()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FractionalNumber
+
+        if (this.shortenFraction().numerator != other.shortenFraction().numerator) return false
+        if (this.shortenFraction().denominator != other.shortenFraction().denominator) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numerator
+        result = 31 * result + denominator
+        return result
+    }
+
+
+    class FractionalNumberValidator(private val fractionalNumber: String) : Validator {
+        companion object {
+            val regexValidator = Regex("""-?\d+(/-?\d*)?""")
+        }
+
+        override fun validate(): Boolean {
+            if (!regexValidator.matches(fractionalNumber))
+                throw IllegalArgumentException("Not fraction in Integers")
+            else
+                return true
+        }
+
     }
 }
--- src/tests/FractionalNumberTest.kt
+++ src/tests/FractionalNumberTest.kt
@@ -4,48 +4,137 @@ import lessons.lesson04.FractionalNumber
 import org.testng.Assert.assertThrows
 import org.testng.AssertJUnit.assertEquals
 import org.testng.annotations.Test
-import java.lang.Exception
+
 @Test
 class FractionalNumberTest {
     @Test
-    fun initWithExceptionTest1(){
-        assertThrows(IllegalArgumentException::class.java){
+    fun initWithExceptionTest1() {
+        assertThrows(IllegalArgumentException::class.java) {
             FractionalNumber("1234УВЫА")
-        }
-    }
-    @Test
-    fun initWithExceptionTest2(){
-        assertThrows(IllegalArgumentException::class.java){
-            FractionalNumber("1234/")
-        }
-    }
-    @Test
-    fun initWithExceptionTest3(){
-        assertThrows(IllegalArgumentException::class.java){
-            FractionalNumber("/1234")
-        }
-    }
-    @Test
-    fun initWithExceptionTest4(){
-        assertThrows(IllegalArgumentException::class.java){
-            FractionalNumber("1234/1234/")
         }
     }
 
     @Test
-    fun toStringTest1(){
-        val fractionalNumber = FractionalNumber("5/3")
-        assertEquals("5/3", fractionalNumber.toString(), )
+    fun initWithExceptionTest2() {
+        assertThrows(IllegalArgumentException::class.java) {
+            FractionalNumber("1234/")
+        }
+    }
+
+    @Test
+    fun initWithExceptionTest3() {
+        assertThrows(IllegalArgumentException::class.java) {
+            FractionalNumber("/1234")
+        }
+    }
+
+    @Test
+    fun initWithExceptionTest4() {
+        assertThrows(IllegalArgumentException::class.java) {
+            FractionalNumber("-1234444444444444/14444444423445454")
+        }
     }
     @Test
-    fun toStringTest2(){
+    fun initWithExceptionTest5() {
+        assertThrows(IllegalArgumentException::class.java) {
+            FractionalNumber("-0/0")
+        }
+    }
+    fun initTest(){
+        val fractionalNumber1 = FractionalNumber("4/5")
+        val newFractionalNumber = FractionalNumber(fractionalNumber1)
+        assertEquals(fractionalNumber1.toString(), newFractionalNumber.toString())
+    }
+    @Test
+    fun toStringTest1() {
+        val fractionalNumber = FractionalNumber("5/3")
+        assertEquals("5/3", fractionalNumber.toString())
+    }
+
+    @Test
+    fun toStringTest2() {
         val fractionalNumber = FractionalNumber("5")
         assertEquals("5/1", fractionalNumber.toString())
     }
 
+    @Test
+    fun toStringTest3() {
+        val fractionalNumber = FractionalNumber("-5")
+        assertEquals("-5/1", fractionalNumber.toString())
+    }
 
     @Test
-    fun toIntTest(){
-        TODO("Not yet implemented")
+    fun shortenFractionTest1() {
+        val fractionalNumber = FractionalNumber("5/10")
+        val shortedFractionalNumber = fractionalNumber.shortenFraction()
+        assertEquals("1/2", shortedFractionalNumber.toString())
+    }
+
+    @Test
+    fun shortenFractionTest2() {
+        val fractionalNumber = FractionalNumber("-4/24")
+        val shortedFractionalNumber = fractionalNumber.shortenFraction()
+        assertEquals("-1/6", shortedFractionalNumber.toString())
+    }
+
+    @Test
+    fun shortenFractionTest3() {
+        val fractionalNumber = FractionalNumber("30/-5")
+        val shortedFractionalNumber = fractionalNumber.shortenFraction()
+        assertEquals("-6/1", shortedFractionalNumber.toString())
+    }
+
+    @Test
+    fun shortenFractionTest4() {
+        val fractionalNumber = FractionalNumber("-12/-3")
+        val shortedFractionalNumber = fractionalNumber.shortenFraction()
+        assertEquals("4/1", shortedFractionalNumber.toString())
+    }
+
+    @Test
+    fun toIntTest() {
+        val fractionalNumber = FractionalNumber("-12/-3")
+        assertEquals(4, fractionalNumber.toInt())
+    }
+    @Test
+    fun toDoubleTest() {
+        val fractionalNumber = FractionalNumber("-3/5")
+        assertEquals(-0.6, fractionalNumber.toDouble())
+    }
+    @Test
+    fun plusTest1(){
+        val fractionalNumberFirst = FractionalNumber("1/2")
+        val fractionalNumberSecond = FractionalNumber("1/3")
+        assertEquals("5/6", (fractionalNumberFirst + fractionalNumberSecond).toString())
+    }
+    @Test
+    fun plusTest2(){
+        val fractionalNumberFirst = FractionalNumber("-1/2")
+        val fractionalNumberSecond = FractionalNumber("1/3")
+        assertEquals("-1/6", (fractionalNumberFirst + fractionalNumberSecond).toString())
+    }
+    fun minusTest(){
+        val fractionalNumberFirst = FractionalNumber("1/2")
+        val fractionalNumberSecond = FractionalNumber("1/6")
+        assertEquals("1/3", (fractionalNumberFirst - fractionalNumberSecond).toString())
+    }
+    fun divTest(){
+        val fractionalNumberFirst = FractionalNumber("1/2")
+        val fractionalNumberSecond = FractionalNumber("1/6")
+        assertEquals("3/1", (fractionalNumberFirst / fractionalNumberSecond).toString())
+    }
+    fun timesTest(){
+        val fractionalNumberFirst = FractionalNumber("4/2")
+        val fractionalNumberSecond = FractionalNumber("1/6")
+        assertEquals("1/3", (fractionalNumberFirst * fractionalNumberSecond).toString())
+    }
+    fun unaryMinusTest() {
+        val fractionalNumber = FractionalNumber("4/2")
+        assertEquals("-2/1", (-fractionalNumber).toString())
+    }
+    fun equalsTest(){
+        val fractionalNumberFirst = FractionalNumber("4/2")
+        val fractionalNumberSecond = FractionalNumber("8/4")
+        assertEquals(true, fractionalNumberFirst == fractionalNumberSecond)
     }
 }
